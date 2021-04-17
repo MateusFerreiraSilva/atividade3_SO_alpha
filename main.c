b330600fe0c1d4a31c46327fdda95ad22e84cc81
MateusFerreiraSilva <ferreiramateus201306@gmail.com>
2021-04-17 12:51:30 -0300

    funcionou disgraça

--- main.c
+++ main.c
@@ -9,25 +9,82 @@
 #include <signal.h>
 #include <pthread.h>
 #include <string.h>
+#include <fcntl.h>
 
 #define MEM_SZ 4096
-#define BUFF_SZ MEM_SZ - sizeof(int) - sizeof(sem_t)
 
 struct shared_area
 {
     sem_t mutex;
     int pid[7];
-    int pNum;
-    queue f1;
+    int pNum, counter;
+    int turn; // turn 0->P5, 1->P6, 2->P7T1, 3->P7T2, 4->P7T3
+    queue f1, f2;
 };
 
 typedef struct shared_area shared_area;
 
 shared_area *shared_area_ptr;
 sem_t thread_mutex;
-int canal[2][2];
+int canal1[2], canal2[2];
 
-void *randConsume(void *args)
+void fechaCanais()
+{
+    close(canal1[1]);
+    close(canal1[1]);
+    close(canal2[0]);
+    close(canal2[0]);
+}
+
+void next_turn()
+{
+    shared_area_ptr->turn = (shared_area_ptr->turn + 1) % 5;
+}
+
+void *f2Consume(void *args)
+{
+    int turn = *((int *)args);
+    queue *q = &shared_area_ptr->f2;
+    int val;
+    while (1)
+    {
+        if (shared_area_ptr->turn == turn)
+        {
+            if (!queue_empty(q) && shared_area_ptr->counter < 1000)
+            {
+                val = queue_pop(q);
+                printf("%d\n", val);
+                fflush(stdout);
+                shared_area_ptr->counter++;
+            }
+            if (shared_area_ptr->counter == 1000)
+                break;
+            next_turn();
+        }
+    }
+    next_turn();
+    pthread_exit(NULL);
+}
+
+// void f2Produce(int val, int turn)
+// {
+//     queue *q = &shared_area_ptr->f2;
+//     while (1)
+//     {
+//         if (shared_area_ptr->turn == turn)
+//         {
+//             if (!queue_full(q))
+//             {
+//                 printf("val %d turn %d\n", val, turn);
+//                 fflush(stdout);
+//                 queue_push(q, val);
+//             }
+//             next_turn();
+//         }
+//     }
+// }
+
+void *f1Consume(void *args)
 {
     int id = *((int *)args);
     queue *q = &shared_area_ptr->f1;
@@ -38,21 +95,28 @@ void *randConsume(void *args)
         if (!queue_empty(q))
         {
             val = queue_pop(q);
-            int idx = id == 1 ? 0 : 1;
-            write(canal[idx][1], &val, sizeof(int));
+            if (id == 1)
+            {
+                write(canal1[1], &val, sizeof(int));
+            }
+            else
+            {
+                write(canal2[1], &val, sizeof(int));
+            }
         }
         sem_post((sem_t *)&thread_mutex);
 
         if (queue_empty(q))
             break;
     }
-    pthread_exit(NULL);
+    sem_post((sem_t *)&shared_area_ptr->mutex);
 }
 
-void randConsumeUtil(int signal) // p2
+void waitSignal()
 {
     pthread_t t[2];
     const int id[2] = {1, 2};
+
     if (sem_init((sem_t *)&thread_mutex, 0, 1) != 0)
     {
         printf("sem_init thread falhou\n");
@@ -60,14 +124,16 @@ void randConsumeUtil(int signal) // p2
     }
 
     sem_wait((sem_t *)&shared_area_ptr->mutex);
+
     for (int i = 0; i < 2; i++)
-        pthread_create(&t[i], NULL, randConsume, (void *)&id[i]);
+        pthread_create(&t[i], NULL, f1Consume, (void *)&id[i]);
     for (int i = 0; i < 2; i++)
         pthread_join(t[i], NULL);
+
     sem_post((sem_t *)&shared_area_ptr->mutex);
 }
 
-void randGerenete()
+void f1Gerenete()
 {
     int id = getpid(), r;
     queue *q = &shared_area_ptr->f1;
@@ -78,12 +144,8 @@ void randGerenete()
         if (!queue_full(q))
         {
             queue_push(q, r);
-            // printf("%d - Processo: %d Escreveu %d\n", queue_size(q), id, r);
             if (queue_full(q))
-            {
-                // printf("Processo %d deu o sinal!!!\n", id);
                 kill(shared_area_ptr->pid[3], SIGUSR1);
-            }
         }
         sem_post((sem_t *)&shared_area_ptr->mutex);
     }
@@ -125,15 +187,24 @@ int main()
 
     // Inicalizacoes!!!!!!!!
     queue_init(&shared_area_ptr->f1);
+    queue_init(&shared_area_ptr->f2);
     int pid[7], status[7];
     memset(shared_area_ptr->pid, -1, sizeof shared_area_ptr->pid);
-    shared_area_ptr->pNum = 6;
-    if (pipe(canal[0]) == -1 || pipe(canal[1]) == -1)
+    shared_area_ptr->pNum = 7;
+    shared_area_ptr->counter = 0;
+    shared_area_ptr->turn = -1; // deixa p5, p6 e p7 parados
+    if (pipe(canal1) == -1 || pipe(canal2) == -1)
     {
         printf("Erro pipe()");
         exit(-1);
     }
+    // else if (fcntl(canal1[0], F_SETFL, O_NONBLOCK) < 0 || fcntl(canal2[0], F_SETFL, O_NONBLOCK) < 0)
+    // {
+    //     printf("Erro pipe()");
+    //     exit(-1);
+    // }
 
+    fflush(stdout);
     for (int i = 0; i < shared_area_ptr->pNum; i++)
     {
         pid[i] = fork();
@@ -142,26 +213,88 @@ int main()
             shared_area_ptr->pid[i] = getpid();
             if (i < 3)
             { // p1 ou p2 ou p3
-                // printf("Processo %d pid %d %d\n", i + 1, getpid(), shared_area_ptr->pid[i]);
-                randGerenete();
+                fechaCanais();
+                f1Gerenete();
             }
             else if (i < 4)
             { // p4
-                // printf("Processo %d pid %d %d\n", i + 1, getpid(), shared_area_ptr->pid[i]);
-                signal(SIGUSR1, randConsumeUtil);
+
+                // fecha canais de leitura
+                close(canal1[0]);
+                close(canal2[0]);
+
+                signal(SIGUSR1, waitSignal);
                 while (1)
                     pause(); // espera pelo sinal
             }
             else if (i < 6)
             { // p5 e p6
+                // fecha canais de escrita
+                close(canal1[1]);
+                close(canal2[1]);
+
+                queue *q = &shared_area_ptr->f2;
+                int turn = i == 4 ? 0 : 1;
                 int val;
-                int idx = i == 4 ? 0 : 1;
-                while (read(canal[idx][0], &val, sizeof(int)))
-                    printf("-->%d P%d\n", val, idx == 0 ? 5 : 6);
+
+                // checa se pipe esta fechado
+
+                if (turn == 0)
+                {
+                    while (1)
+                    {
+                        if (shared_area_ptr->turn == turn)
+                        {
+                            if (read(canal1[0], &val, sizeof(int)) > 0)
+                            {
+                                // printf("read %d turn %d\n", val, turn);
+                                // fflush(stdout);
+                                if (!queue_full(q))
+                                {
+                                    val = queue_push(q, val);
+                                    // printf("%d\n", queue_rear(q));
+                                    // fflush(stdout);
+                                }
+                            }
+                            next_turn();
+                        }
+                    }
+                }
+
+                else if (turn == 1)
+                {
+                    while (1)
+                    {
+                        if (shared_area_ptr->turn == turn)
+                        {
+                            if (read(canal2[0], &val, sizeof(int)) > 0)
+                            {
+                                // printf("read %d turn %d\n", val, turn);
+                                // fflush(stdout);
+                                if (!queue_full(q))
+                                {
+                                    val = queue_push(q, val);
+                                    // printf("%d\n", queue_rear(q));
+                                    // fflush(stdout);
+                                }
+                            }
+                            next_turn();
+                        }
+                    }
+                }
             }
-            else if (i == 6)
-            {
-                i = i;
+            else
+            { // p7
+                fechaCanais();
+                pthread_t t[3];
+                const int turn[3] = {2, 3, 4};
+                for (int i = 0; i < 3; i++)
+                    pthread_create(&t[i], NULL, f2Consume, (void *)&turn[i]);
+                for (int i = 0; i < 3; i++)
+                    pthread_join(t[i], NULL);
+
+                printf("counter %d\n", shared_area_ptr->counter);
+                exit(status[i]);
             }
         }
     }
@@ -176,6 +309,7 @@ int main()
         if (counter == shared_area_ptr->pNum)
         {
             sem_post((sem_t *)&shared_area_ptr->mutex); // abre o semaforo para liberar p1, p2, p3 e p4
+            shared_area_ptr->turn = 0;
             break;
         }
         else
--- test.c
+++ test.c
@@ -1,28 +1,53 @@
 #include <stdio.h>
-#include <pthread.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include "queue.h"
-
-void *func(void *args)
-{
-    int *val = args;
-    printf("%d\n", *val);
-    pthread_exit(NULL);
-}
+#include <semaphore.h>
+#include <signal.h>
+#include <pthread.h>
+#include <string.h>
 
 int main()
 {
-    while (1)
+    int p[2];
+    pipe(p);
+    int pid, status;
+    pid = fork();
+    if (pid == 0)
     {
+        close(p[0]);
+        int i = 0;
+        while (1)
+        {
+            write(p[1], &i, sizeof(int));
+            // printf("write %d\n", i);
+            i++;
+        }
     }
-    pthread_t t[2];
-    const int val[2] = {40, 45};
-    for (int i = 0; i < 2; i++)
+    else
     {
-        pthread_create(&t[i], NULL, func, (void *)&val[i]);
-    }
+        close(p[1]);
 
-    for (int i = 0; i < 2; i++)
-    {
-        pthread_join(t[i], NULL);
+        int i;
+        while (1)
+        {
+            read(p[0], &i, sizeof(int));
+            printf("read %d\n", i);
+            fflush(stdout);
+            // buffer[aux++] = i;
+            // if (aux == 1000)
+            // {
+            //     for (int j = 0; j < 1000; j++)
+            //     {
+            //         printf("read %d\n", buffer[j]);
+            //     }
+            //     aux = 0;
+            // }
+        }
+
+        pid = wait(&status);
     }
 }
